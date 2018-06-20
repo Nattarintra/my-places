82ac02f04f5699ed323bf01191f86136b2f894b2
Nattarintra <34267102+Nattarintra@users.noreply.github.com>
2018-06-20 11:21:33 +0200

    Google maps Markers from day 11-12

---
+++ my-places/includes/class-my-places-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       wordpress.nattarintrathutsanti.se
+ * @since      1.0.0
+ *
+ * @package    My_Places
+ * @subpackage My_Places/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    My_Places
+ * @subpackage My_Places/includes
+ * @author     Nattarintra Thutsanti <nattarintra.thutsant@medieinstitutet.se>
+ */
+class My_Places_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
